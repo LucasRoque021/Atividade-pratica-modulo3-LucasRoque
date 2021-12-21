c1d138e7805fce2d8f8ed3b209ed429ad14affec
= <=>
2021-12-21 00:45:07 -0300

    atividade pratica crud mvc recode

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LucasRoque_atividadePratica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
